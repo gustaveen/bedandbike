a9f40a5d86629fce3f8f96e7ae287407e8f8f9bb
Gustav <gustav.een@tink.se>
2019-08-07 12:39:28 +0200

    Add map section

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,65 +1,101 @@
 import React from 'react';
-import { FormattedHTMLMessage } from 'react-intl';
-import map from '../../assets/images/map_crop.jpg';
+import { FormattedMessage } from 'react-intl';
+import map from '../../assets/images/map-pin.jpg';
 // import bike from '../../assets/images/bike.png';
 
 import styled from 'styled-components';
 import { breakpoints } from '../breakpoints';
 
 const MapRoot = styled.div`
-  background-color: #f9f9f9;
+  position: relative;
+  background-color: #aadbff;
+  background-image: url(${map});
+  background-repeat: no-repeat;
+  background-size: 100%;
 `;
 
 const MapContainer = styled.div`
+  display: flex;
+  position: relative;
+  max-width: 1220px;
+  margin-left: 4%;
+  margin-right: 4%;
+  padding-top: 10%;
+  padding-bottom: 10%;
+`;
+
+const MapInfo = styled.div`
   max-width: 400px;
   margin-left: auto;
   margin-right: auto;
-  padding-top: 32px;
-  padding-bottom: 32px;
+  padding: 32px;
   text-align: center;
+  background-color: white;
 
   @media ${breakpoints.ml} {
-    padding-top: 32px;
-    padding-bottom: 32px;
+    /* position: absolute;
+    top: 50%;
+    right: 20%; */
+    margin-right: unset;
+    padding: 40px;
+    /* transform: translateY(-50%); */
   }
 
   @media ${breakpoints.xl} {
-    padding-top: 96px;
-    padding-bottom: 96px;
+    padding: 90px 50px;
   }
 `;
 
-const MapIcon = styled.img`
-  display: inline-block;
-  margin-bottom: 20px;
-`;
+// const MapIcon = styled.img`
+//   display: inline-block;
+//   margin-bottom: 20px;
+// `;
 
-const MapText = styled.h2`
-  font-size: 1.6rem;
-  line-height: 1.4;
-  padding-left: 2rem;
-  padding-right: 2rem;
-
-  @media ${breakpoints.md} {
-    font-size: 2rem;
-  }
+const MapTitle = styled.h3`
+  margin-bottom: 8px;
+  font-size: 2rem;
+  font-weight: bold;
 
   @media ${breakpoints.ml} {
-    font-size: 2.2rem;
-  }
-
-  @media ${breakpoints.lg} {
-    font-size: 2.6rem;
+    font-size: 2.4rem;
   }
 `;
 
-const Map = ({ text }) => {
+const MapText = styled.p`
+  margin-bottom: 20px;
+  font-size: 1.8rem;
+  color: ${props => props.theme.palette.gray.dark};
+  @media ${breakpoints.ml} {
+    margin-bottom: 30px;
+  }
+`;
+
+const MapLink = styled.a`
+  color: ${props => props.theme.palette.blue.main};
+  font-size: 1.8rem;
+`;
+
+const Map = () => {
   return (
     <MapRoot>
-      <img src={map} alt="te" />
+      {/* <img src={map} alt="te" /> */}
       <MapContainer>
-        {/* <MapIcon src={bike} alt="logo" /> */}
-        <MapText>{text}</MapText>
+        <MapInfo>
+          {/* <MapIcon src={bike} alt="logo" /> */}
+          <MapTitle>
+            <FormattedMessage id="mapTitle" />
+          </MapTitle>
+          <MapText>
+            <FormattedMessage id="mapText" />
+          </MapText>
+          <MapLink
+            href="https://goo.gl/maps/Beyf4Yn9HdjYM6GLA"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FormattedMessage id="mapLink" />
+          </MapLink>
+        </MapInfo>
       </MapContainer>
     </MapRoot>
   );
